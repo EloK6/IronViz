ebe319434c9186f963cc4f2f8c5bb9bdfa76c36b
EloK6 <ecassignol@gmail.com>
2019-03-26 21:33:14 +0100

    First Page

--- client-ironviz/src/App.js
+++ client-ironviz/src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+import HomePage from "./components/HomePage";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          {/* <Route exact path="/beers" component={Indicators} />
+          <Route exact path="/beers/:id" component={Countries} />
+          <Route exact path="/random-beer" component={MakeChart} />
+          <Route exact path="/new-beer" component={Profile} /> */}
+        </Switch>
       </div>
     );
   }
---
+++ client-ironviz/src/components/HomePage.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="HomePage">
+        <h1>IronViz</h1>
+        <p>
+          Hae duae provinciae bello quondam piratico catervis mixtae praedonum a
+          Servilio pro consule missae sub iugum factae sunt vectigales. et hae
+          quidem regiones velut in prominenti terrarum lingua positae ob orbe
+          eoo monte Amano disparantur. Superatis Tauri montis verticibus qui ad
+          solis ortum sublimius attolluntur, Cilicia spatiis porrigitur late
+          distentis dives bonis omnibus terra, eiusque lateri dextro adnexa
+          Isauria, pari sorte uberi palmite viget et frugibus minutis, quam
+          mediam navigabile flumen Calycadnus interscindit.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
