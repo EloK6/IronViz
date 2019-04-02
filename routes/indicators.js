a88355a4bd43985ec67ea3e8822a9185c0484ad4
EloK6 <ecassignol@gmail.com>
2019-04-02 12:52:18 +0200

    Route Indicators ok

--- routes/indicators.js
+++ routes/indicators.js
@@ -5,24 +5,16 @@ const router = express.Router();
 const Country = require("../models/Country");
 const Indicator = require("../models/Indicator");
 
-router.get("/indicators/:country_id", (req, res, next) => {
-  Indicator.findById(req.params.country_id)
-    .then(response => {
-      res.status(200).json(response);
+router.get("/indicators/:countryId", function(req, res, next) {
+  console.log("params", req.params);
+  Indicator.find({ country_id: req.params.countryId })
+    .populate("Country")
+    .then(allIndicators => {
+      res.json(allIndicators);
     })
     .catch(err => {
       res.json(err);
     });
 });
-
-// router.get("/indicators", (req, res, next) => {
-//   Indicator.find()
-//     .then(response => {
-//       res.status(200).json(response);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
 
 module.exports = router;
