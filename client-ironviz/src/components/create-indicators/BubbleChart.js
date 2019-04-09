8e5fbb612d3b1dac91ad79f88f51258505b4a8be
EloK6 <ecassignol@gmail.com>
2019-04-09 23:02:06 +0200

    Adjust

--- client-ironviz/src/components/create-indicators/BubbleChart.js
+++ client-ironviz/src/components/create-indicators/BubbleChart.js
@@ -2,6 +2,144 @@ import React, { Component } from "react";
 import * as d3 from "d3";
 import axios from "axios";
 
+class BubbleChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  getData = () => {
+    axios.get(`http://localhost:5000/api/countries`).then(responseFromApi => {
+      this.setState({
+        data: responseFromApi.data
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.updateChart();
+    this.getData();
+  }
+
+  componentDidUpdate() {
+    this.updateChart();
+  }
+
+  updateChart() {
+    const xAxisValue = this.props.xAxisValue;
+    const yAxisValue = this.props.yAxisValue;
+    const rAxisValue = this.props.rAxisValue;
+
+    //Inner margins of the chart
+    const innerMargin = { top: 20, bottom: 40, left: 20, right: 20 };
+    const innerWidth = this.props.width - innerMargin.left - innerMargin.right;
+    const innerHeight =
+      this.props.height - innerMargin.top - innerMargin.bottom;
+
+    //xScale
+    let xMax = d3.max(
+      this.state.data.map(d =>
+        d.indicator_id.find(indic => indic.key === xAxisValue)
+      ),
+      d => d.value
+    );
+
+    let xScale = d3
+      .scaleLinear()
+      .domain([0, xMax])
+      .range([innerMargin.left, innerMargin.left + innerWidth]);
+
+    //yScale
+    let yMax = d3.max(
+      this.state.data.map(d =>
+        d.indicator_id.find(indic => indic.key === yAxisValue)
+      ),
+      d => d.value
+    );
+
+    let yScale = d3
+      .scaleLinear()
+      .domain([0, yMax])
+      .range([innerMargin.top + innerHeight, innerMargin.top]);
+
+    //Radius
+    let maxRadius = d3.max(
+      this.state.data.map(d =>
+        d.indicator_id.find(indic => indic.key === rAxisValue)
+      ),
+      d => d.value
+    );
+    let rScale = d3
+      .scaleLinear()
+      .domain([1, maxRadius])
+      .range([5, 60]);
+
+    //Color
+    let color = d3
+      .scaleOrdinal()
+      .domain([this.state.data.map(d => d.region)])
+      .range(["#FF8370", "#AA66E8", "#7DDAFF", "#68E866", "#FFE36B"]);
+
+    // Add X axis
+    var x = d3
+      .scaleLinear()
+      .domain([0, xMax])
+      .range([0, this.props.width]);
+
+    //Add Y axis
+    var y = d3
+      .scaleLinear()
+      .domain([0, yMax])
+      .range([this.props.height, 0]);
+
+    //dataviz
+    d3.select(this.svgEl)
+      .selectAll("circle")
+      .data(this.state.data)
+      .transition()
+      .duration(2000)
+      .attr("cx", d =>
+        xScale(d.indicator_id.find(indic => indic.key === xAxisValue).value)
+      )
+      .attr("cy", d =>
+        yScale(d.indicator_id.find(indic => indic.key === yAxisValue).value)
+      )
+      .attr("r", d => {
+        let found = d.indicator_id.find(indic => {
+          return indic.key === rAxisValue;
+        });
+        return rScale(found.value);
+      })
+      .style("fill", d => color(d.region))
+      .style("opacity", 0.65)
+      .style("stroke", "white");
+  }
+
+  render() {
+    let points = this.state.data.map(d => (
+      <circle cx="0.5" cy="0.5" style={{ fill: "white" }} />
+    ));
+
+    return (
+      <div className="BubbleChart">
+        <svg
+          preserveAspectRatio="xMidYMid meet"
+          viewBox={`0 0 ${this.props.width} ${this.props.height}`}
+          width={this.props.width}
+          height={this.props.height}
+          ref={element => (this.svgEl = element)}
+        >
+          {points}
+        </svg>
+      </div>
+    );
+  }
+}
+
+export default BubbleChart;
+
 // class BubbleChart extends Component {
 //   constructor(props) {
 //     super(props);
@@ -147,145 +285,3 @@ import axios from "axios";
 // }
 
 // export default BubbleChart;
-
-class BubbleChart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: []
-    };
-  }
-
-  getData = () => {
-    axios.get(`http://localhost:5000/api/countries`).then(responseFromApi => {
-      this.setState({
-        data: responseFromApi.data
-      });
-    });
-  };
-
-  componentDidMount() {
-    this.updateChart();
-    this.getData();
-  }
-
-  componentDidUpdate() {
-    console.log("componentDidUpdate");
-
-    this.updateChart();
-  }
-
-  updateChart() {
-    const xAxisValue = this.props.xAxisValue;
-    const yAxisValue = this.props.yAxisValue;
-    const rAxisValue = this.props.rAxisValue;
-
-    //Inner margins of the chart
-    const innerMargin = { top: 20, bottom: 40, left: 20, right: 20 };
-    const innerWidth = this.props.width - innerMargin.left - innerMargin.right;
-    const innerHeight =
-      this.props.height - innerMargin.top - innerMargin.bottom;
-
-    //xScale
-    let xMax = d3.max(
-      this.state.data.map(d =>
-        d.indicator_id.find(indic => indic.key === xAxisValue)
-      ),
-      d => d.value
-    );
-
-    let xScale = d3
-      .scaleLinear()
-      .domain([0, xMax])
-      .range([0, this.props.width]);
-
-    //yScale
-    let yMax = d3.max(
-      this.state.data.map(d =>
-        d.indicator_id.find(indic => indic.key === yAxisValue)
-      ),
-      d => d.value
-    );
-
-    let yScale = d3
-      .scaleLinear()
-      .domain([0, yMax])
-      .range([innerMargin.top + innerHeight, innerMargin.top]);
-
-    //Radius
-    let maxRadius = d3.max(
-      this.state.data.map(d =>
-        d.indicator_id.find(indic => indic.key === rAxisValue)
-      ),
-      d => d.value
-    );
-    let rScale = d3
-      .scaleLinear()
-      .domain([1, maxRadius])
-      .range([5, 60]);
-
-    //Color
-    let color = d3
-      .scaleOrdinal()
-      .domain([this.state.data.map(d => d.region)])
-      .range(["#FF8370", "#AA66E8", "#7DDAFF", "#68E866", "#FFE36B"]);
-
-    // Add X axis
-    var x = d3
-      .scaleLinear()
-      .domain([0, xMax])
-      .range([0, this.props.width]);
-
-    //Add Y axis
-    var y = d3
-      .scaleLinear()
-      .domain([0, yMax])
-      .range([this.props.height, 0]);
-
-    //dataviz
-    d3.select(this.svgEl)
-      .selectAll("circle")
-      .data(this.state.data)
-      .transition()
-      .duration(2000)
-      .attr("cx", d =>
-        xScale(d.indicator_id.find(indic => indic.key === xAxisValue).value)
-      )
-      .attr("cy", d =>
-        yScale(d.indicator_id.find(indic => indic.key === yAxisValue).value)
-      )
-      .attr("r", d => {
-        let found = d.indicator_id.find(indic => {
-          return indic.key === rAxisValue;
-        });
-        return rScale(found.value);
-      })
-      .style("fill", d => color(d.region))
-      .style("opacity", 0.65)
-      .style("stroke", "white");
-  }
-
-  render() {
-    console.log("render");
-
-    let points = this.state.data.map(d => (
-      <circle cx="0.5" cy="0.5" style={{ fill: "white" }} />
-    ));
-
-    return (
-      <div className="BubbleChart">
-        <svg
-          preserveAspectRatio="xMidYMid meet"
-          viewBox={`0 0 ${this.props.width} ${this.props.height}`}
-          width={this.props.width}
-          height={this.props.height}
-          ref={element => (this.svgEl = element)}
-        >
-          {points}
-        </svg>
-      </div>
-    );
-  }
-}
-
-export default BubbleChart;
