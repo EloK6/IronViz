5da4d2d2db1cd2c442332cb83df573cf3779195b
EloK6 <ecassignol@gmail.com>
2019-04-09 23:01:53 +0200

    Adjust Bubble Chart

--- client-ironviz/src/components/dataviz/Chart.js
+++ client-ironviz/src/components/dataviz/Chart.js
@@ -3,42 +3,66 @@ import * as d3 from "d3";
 // import axios from "axios";
 
 class Chart extends Component {
-  // let nodes = d3.range(100).map(function(d) {
-  // return {radius: Math.random() * 25}
-  // })
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  // let simulation = d3.forceSimulation(nodes)
-  // .force('charge', d3.forceManyBody().strength(5))
-  // .force('center', d3.forceCenter(width / 2, height / 2))
-  // .force('collision', d3.forceCollide().radius(function(d) {
-  //   return d.radius
-  // }))
-  // .on('tick', ticked);
+  componentDidMount() {
+    this.updateChart();
+    // this.getData();
+  }
 
-  // function ticked() {
-  //   var u = d3.select(this.svgEl)
-  //     .selectAll('circle')
-  //     .data(nodes)
+  componentDidUpdate() {
+    this.updateChart();
+  }
 
-  //   u.enter()
-  //     .append('circle')
-  //     .attr('r', function(d) {
-  //       return d.radius
-  //     })
-  //     .merge(u)
-  //     .attr('cx', function(d) {
-  //       return d.x
-  //     })
-  //     .attr('cy', function(d) {
-  //       return d.y
-  //     })
+  updateChart() {
+    var numNodes = 100;
+    var nodes = d3.range(numNodes).map(function(d) {
+      return { radius: Math.random() * 25 };
+    });
 
-  //   u.exit().remove()
-  // }
+    let simulation = d3
+      .forceSimulation(nodes)
+      .force("charge", d3.forceManyBody().strength(5))
+      .force(
+        "center",
+        d3.forceCenter(this.props.width / 2, this.props.height / 2)
+      )
+      .force(
+        "collision",
+        d3.forceCollide().radius(function(d) {
+          return d.radius;
+        })
+      )
+      .on("tick", ticked);
 
+    function ticked() {
+      var u = d3
+        .select(this.svgEl)
+        .selectAll("circle")
+        .data(nodes);
+
+      u.enter()
+        .append("circle")
+        .attr("r", function(d) {
+          return d.radius;
+        })
+        .merge(u)
+        .attr("cx", function(d) {
+          return d.x;
+        })
+        .attr("cy", function(d) {
+          return d.y;
+        });
+
+      u.exit().remove();
+    }
+  }
   render() {
     return (
-      <div className="BubbleChart">
+      <div className="Chart">
         <svg
           preserveAspectRatio="xMidYMid meet"
           viewBox={`0 0 ${this.props.width} ${this.props.height}`}
