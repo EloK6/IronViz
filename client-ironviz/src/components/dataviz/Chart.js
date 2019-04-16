7199585bb116efe1dc4cf728b8f904a7bd0c5881
EloK6 <ecassignol@gmail.com>
2019-04-16 20:02:11 +0200

    chart works

--- client-ironviz/src/components/dataviz/Chart.js
+++ client-ironviz/src/components/dataviz/Chart.js
@@ -1,38 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import * as d3 from "d3";
 import axios from "axios";
 import _ from "lodash";
-
-function Node() {
-  const MAXVALUE = 1000000;
-  this.x = 0;
-  this.y = 0;
-  this.value = Math.random() * MAXVALUE;
-  this.radius = Math.random() * 25;
-}
-const json = [...Array(195)].map(el => new Node());
 
 class Chart extends React.Component {
   static defaultProps = {
     width: 800,
     height: 600,
     forceStrength: 0.02,
-    velocityDecay: 0.1
+    velocityDecay: 0.05
   };
 
   constructor(props) {
     super(props);
-
-    const { width, height, forceStrength, velocityDecay } = this.props;
-
+    this.nodes = [];
     this.state = {
-      nodes: json
+      nodes: []
     };
+  }
 
-    //
+  init() {
+    const { width, height, forceStrength, velocityDecay } = this.props;
     // d3-force simulation
-    //
-
     function charge(d) {
       return -forceStrength * Math.pow(d.radius, 2);
     }
@@ -63,6 +52,51 @@ class Chart extends React.Component {
       .on("tick", this.ticked);
   }
 
+  getData = () => {
+    axios.get(`http://localhost:5000/api/countries`).then(responseFromApi => {
+      this.setState({
+        nodes: this.createNodes(responseFromApi.data)
+      });
+      this.init();
+    });
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  // // //Function Node
+  createNodes = rawData => {
+    //radius
+    let maxRadius = d3.max(
+      rawData.map(d =>
+        d.indicator_id.find(indic => indic.key === "population")
+      ),
+      d => d.value
+    );
+    let radiusScale = d3
+      .scaleLinear()
+      .domain([1, maxRadius])
+      .range([5, 120]);
+
+    // Use map() to convert raw data into node data.
+    const myNodes = rawData.map(d => ({
+      id: d._id,
+      radius: radiusScale(
+        d.indicator_id.find(indic => indic.key === "population").value
+      ),
+      value: d.indicator_id.find(indic => indic.key === "population").value,
+      name: d.name,
+      region: d.region,
+      x: Math.random() * this.props.width,
+      y: Math.random() * this.props.height
+    }));
+
+    // sort them descending to prevent occlusion of smaller nodes.
+    myNodes.sort((a, b) => b.value - a.value);
+    return myNodes;
+  };
+
   ticked = () => {
     // https://stackoverflow.com/a/46865234/133327
     this.setState({ nodes: _.cloneDeep(this.nodes) });
@@ -78,9 +112,9 @@ class Chart extends React.Component {
         viewBox={`0 0 ${width} ${height}`}
         preserveAspectRatio="xMidYMid meet"
       >
-        {this.state.nodes.map(node => (
+        {this.nodes.map(node => (
           <circle
-            key={node.id}
+            key={node.name}
             r={node.radius}
             cx={node.x}
             cy={node.y}
