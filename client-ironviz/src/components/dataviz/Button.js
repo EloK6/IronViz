1d432a03e0705bc73142362e60c0ec0d6944e0b5
EloK6 <ecassignol@gmail.com>
2019-04-17 18:18:21 +0200

    Page dataviz WIP

---
+++ client-ironviz/src/components/dataviz/Button.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+  onBtnClick = event => {
+    this.props.onChanged(event.target.name);
+  };
+  render() {
+    const { active } = this.props;
+    return (
+      <div className="Button">
+        <button
+          className={`Button__form ${active === "all" && "active"}`}
+          name="all"
+          onClick={this.onBtnClick}
+        >
+          All
+        </button>
+        <button
+          className={`Button__form ${active === "region" && "active"}`}
+          name="region"
+          onClick={this.onBtnClick}
+        >
+          Region
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Button;
--- client-ironviz/src/components/dataviz/Dataviz.js
+++ client-ironviz/src/components/dataviz/Dataviz.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
 import Chart from "./Chart";
+import Button from "./Button";
 
 class Dataviz extends Component {
   render() {
@@ -9,6 +10,7 @@ class Dataviz extends Component {
       <div className="Dataviz">
         <NavBar />
         <div className="Dataviz__content">
+          <Button />
           <Chart />
         </div>
         <Footer />
